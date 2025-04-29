8cb9da9453c2e2a98a18bf66c5421ed47a93cc5f
Tobias Mareco <tobiasmareco2002@gmail.com>
2025-04-29 16:41:18 +0000

    using custom json models and show in the view

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,10 +1,29 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
 ], (Controller) => {
     "use strict";
 
     return Controller.extend("tobiasmareco.invoices.controller.MainView", {
         onInit() {
+            const oJsonModel = new sap.ui.model.json.JSONModel();
+            const oView = this.getView();
+            oJsonModel.loadData("../model/json/selectionScreenMenu.json");
+            oView.setModel(oJsonModel,"selectionScreen");
+
+            const oStatusJsonModel = new sap.ui.model.json.JSONModel();;
+            oStatusJsonModel.loadData('../model/json/status.json');
+            oView.setModel(oStatusJsonModel,"status");
+        },
+        onFilter(oEvent){
+
+        },
+        onClearFilter(oEnvent){
+            const oModelSelectionScreen = this.getView().getModel("selectionScreen");
+            oModelSelectionScreen.setProperty("/CountryKey","");
+            oModelSelectionScreen.setProperty("/shipName","");
         }
     });
 });
