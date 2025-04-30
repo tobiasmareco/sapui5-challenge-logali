7a6a42381b2642c5ac61a7613c8b41e724205438
Tobias Mareco <tobiasmareco2002@gmail.com>
2025-04-30 21:42:20 +0000

    end all funtionabilities

--- webapp/controller/View.controller.js
+++ webapp/controller/View.controller.js
@@ -10,7 +10,17 @@ sap.ui.define([
      */
     (Controller, JSONModel) => {
         "use strict";
-        let oDataSimulator = [];
+        let oDataSimulator = {
+            users: []
+        };
+        function checkIsEmpty(obj) {
+            for (const [key, value] of Object.entries(obj)) {
+                if (value == "") {
+                    return true;
+                }
+                return false;
+            }
+        }
         const MODEL_PATH = '../model/JSON/';
         return Controller.extend("tobiasmareco.form.controller.View", {
             onInit() {
@@ -35,11 +45,56 @@ sap.ui.define([
                 const FORM_REGISTER_DATA = new JSONModel();
                 FORM_REGISTER_DATA.loadData(`${MODEL_PATH}formRegisterData.json`);
                 this.getView().setModel(FORM_REGISTER_DATA, 'formDataRegister');
-                
+
+                const SHOW_LIST_USERS = new JSONModel();
+                SHOW_LIST_USERS.setData(oDataSimulator);
+                this.getView().setModel(SHOW_LIST_USERS, 'oDataSimulator');
+
+                console.log(this.getView().getModel('mCivilStatus').getData());
             },
             saveDataRegister() {
                 const data = this.getView().getModel('formDataRegister').getData();
-                console.log(data)
+
+                const isEmpty = checkIsEmpty(data);
+                console.log(isEmpty);
+                if (isEmpty) {
+                    return;
+                }
+
+                oDataSimulator.users.push(data);
+                console.log(oDataSimulator);
+                const NEW_LIST_USERS = new JSONModel();
+                NEW_LIST_USERS.setData(oDataSimulator);
+                this.getView().setModel(NEW_LIST_USERS, 'oDataSimulator');
+            },
+            onChangeSelectNationality(e) {
+                const { Nationality } = this.getView().getModel('formDataRegister').getData();
+                const { Nations } = this.getView().getModel('mCountries').getData();
+                Nations?.forEach(nation => {
+                    if (nation?.key == Nationality) {
+                        console.log(nation)
+                        const oJModelPlaceBirth = new JSONModel();
+                        oJModelPlaceBirth.setData(nation);
+                        this.getView().setModel(oJModelPlaceBirth, 'mPlaceBirth');
+                    }
+                });
+            },
+            onChangeSelectCountry(e) {
+                const { Country } = this.getView().getModel('formDataRegister').getData();
+                const { Nations } = this.getView().getModel('mCountries').getData();
+                Nations?.forEach(nation => {
+                    if (nation?.key == Country) {
+                        console.log(nation)
+                        const oJModelProvince = new JSONModel();
+                        oJModelProvince.setData(nation);
+                        this.getView().setModel(oJModelProvince, 'mAddress');
+                    }
+                });
+            },
+            clearDataRegister() {
+                const CLEAR_FORM = new JSONModel();
+                CLEAR_FORM.setData({});
+                this.getView().setModel(CLEAR_FORM, 'formDataRegister');
             }
         });
     });
